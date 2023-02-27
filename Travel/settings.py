b7be876cb63b281aa3c7a9c5416e2f744a43e48a
coded <coded@example.com>
2023-02-27 08:30:54 -0800

    updated

--- Travel/settings.py
+++ Travel/settings.py
@@ -85,12 +85,28 @@ WSGI_APPLICATION = 'Travel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.postgresql',
+        'NAME'     : 'railway',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'qpyHg8C9Jv6I2F08IWH7',
+        'HOST'     : 'containers-us-west-158.railway.app',
+        'PORT'     : '7530',
     }
 }
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
