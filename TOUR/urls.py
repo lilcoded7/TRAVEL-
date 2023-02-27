acfbca5f91f67f488bd651673889ff6bd9d25f42
coded <coded@example.com>
2023-02-27 07:11:34 -0800

    updated

--- TOUR/urls.py
+++ TOUR/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('home', views.index)
+    
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('register', views.register, name='register'),
+    path('services', views.service, name='services'),
+    path('contact', views.contact, name='contact')
 
 ]
--- TOUR/views.py
+++ TOUR/views.py
@@ -3,11 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 
+
+
+
 def home(request):
     
     return render(request, 'TOUR/home.html')
 
+def contact(request):
+    return render(request, 'TOUR/contact.html')
 
-def index(request):
-    
-    return render(request, 'TOUR/index.html')
+def about(request):
+
+    return render(request, 'TOUR/about.html')
+
+def service(request):
+
+    return render(request, 'TOUR/services.html')
+
+def register(request):
+
+    return render(request, 'TOUR/register.html')
