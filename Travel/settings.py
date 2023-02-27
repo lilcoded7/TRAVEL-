608d2340557a19261e90ed5dd6f7dd269819bc63
coded <coded@example.com>
2023-02-27 08:12:09 -0800

    updated

--- Travel/settings.py
+++ Travel/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'TOUR',
+    "corsheaders",
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -72,6 +74,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Travel.wsgi.application'
+
+
+
+# CORS_ALLOWED_ORIGINS = [
+    
+# ]
 
 
 # Database
@@ -121,7 +129,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL  = 'images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 STATICFILES_DIRS =  os.path.join(BASE_DIR, 'static'),
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
